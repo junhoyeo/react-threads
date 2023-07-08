2f248ad6a7dde665695650060c2b8044f0024f32
Junho Yeo <i@junho.io>
2023-07-08 16:25:27 +0900

    Add landing page draft

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,26 @@
 import { NextPage } from 'next';
+import { BannerCTA } from '@/components/BannerCTA';
 
 const Home: NextPage = () => {
-  return <main className="flex flex-col items-center justify-center w-full h-full min-h-screen"></main>;
+  return (
+    <main className="flex flex-col items-center justify-center h-full min-h-screen py-[36px]">
+      <div className="flex flex-col w-full max-w-2xl mx-auto">
+        <header className="flex flex-col items-center gap-3 px-4 py-4">
+          <h1 className="text-2xl text-white">React Threads</h1>
+          <div className="border-[0.9px] border-[rgba(243,245,247,0.15)] rounded-[8px] w-full p-2 px-4 text-zinc-400/60">
+            Library coming soon...
+          </div>
+          <a href="/CuW23qzhIdJ">
+            <button className="border-[0.9px] border-[rgba(243,245,247,0.15)] rounded-[10px] px-4 text-[rgb(243,245,247)] font-semibold text-[15px] h-[34px] transition-transform focus:scale-95">
+              Go to Example
+            </button>
+          </a>
+        </header>
+
+        <BannerCTA />
+      </div>
+    </main>
+  );
 };
 
 export default Home;
