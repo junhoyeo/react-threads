643ff5db930d9b03c6d399c9b80735a11f9cb02a
Junho Yeo <i@junho.io>
2023-07-08 20:35:50 +0900

    [app] Remove old box

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -7,9 +7,6 @@ const Home: NextPage = () => {
       <div className="flex flex-col w-full max-w-2xl mx-auto">
         <header className="flex flex-col items-center gap-3 px-4 py-4">
           <h1 className="text-2xl text-white">React Threads</h1>
-          <div className="border-[0.9px] border-[rgba(243,245,247,0.15)] rounded-[8px] w-full p-2 px-4 text-zinc-400/60">
-            Library coming soon...
-          </div>
           <a href="/CuW23qzhIdJ">
             <button className="border-[0.9px] border-[rgba(243,245,247,0.15)] rounded-[10px] px-4 text-[rgb(243,245,247)] font-semibold text-[15px] h-[34px] transition-transform focus:scale-95">
               Go to Example
