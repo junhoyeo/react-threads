13dbd41d27f37820e9f3672504a1e3a91fc913c0
Junho Yeo <i@junho.io>
2023-07-16 14:36:42 +0900

    [app] Remove `axiosOptions` that was causing request to fail

--- app/src/pages/[threadId].tsx
+++ app/src/pages/[threadId].tsx
@@ -3,37 +3,14 @@ import { Thread } from 'react-threads';
 import { Thread as ThreadPost, ThreadsAPI } from 'threads-api';
 import { BannerCTA } from '@/components/BannerCTA';
 
-const threadsAPI = new ThreadsAPI();
+const threadsAPI = new ThreadsAPI({
+  verbose: true,
+  deviceID: `android-yopkn1wlen40000`,
+});
 
 type Props = {
   threadID: string;
   thread: ThreadPost;
-};
-
-const axiosOptions = {
-  headers: {
-    authority: 'www.threads.net',
-    'cache-control': 'no-cache',
-    origin: 'https://www.threads.net',
-    accept:
-      'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-    'accept-language': 'ko,en;q=0.9,ko-KR;q=0.8,ja;q=0.7',
-    pragma: 'no-cache',
-    referer: 'https://www.instagram.com/',
-    'sec-fetch-dest': 'document',
-    'sec-fetch-mode': `navigate`,
-    'sec-fetch-site': `cross-site`,
-    'sec-fetch-user': `?1`,
-    'upgrade-insecure-requests': `1`,
-    'sec-ch-prefers-color-scheme': 'dark',
-    'sec-ch-ua': '"Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114"',
-    'sec-ch-ua-full-version-list': `"Not.A/Brand";v="8.0.0.0", "Chromium";v="114.0.5735.198", "Google Chrome";v="114.0.5735.198"'`,
-    'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-platform': '"macOS"',
-    'sec-ch-ua-platform-version': '"13.0.0"',
-    'user-agent': `Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36`,
-    'viewport-width': `536`,
-  },
 };
 
 export const getStaticProps: GetStaticProps<Props, { threadId: string }> = async (context) => {
@@ -52,7 +29,7 @@ export const getStaticProps: GetStaticProps<Props, { threadId: string }> = async
       );
       return { notFound: true };
     }
-    const thread = await threadsAPI.getThreads(postID, axiosOptions);
+    const thread = await threadsAPI.getThreads(postID);
     const { containing_thread } = thread;
 
     return {
